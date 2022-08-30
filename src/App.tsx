c4e8d8e18724280c14b315c32e1eff49a0ca44f4
Luís Gomes <luis252001@hotmail.com>
2022-08-29 23:15:39 -0300

    Apply border radius on input value change

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App: React.FC = () => {
+  const [radius, setRadius] = useState({
+    topLeftRadius: "",
+    topRightRadius: "",
+    bottomLeftRadius: "",
+    bottomRightRadius: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRadius({ ...radius, [e.target.name]: e.target.value })
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app">
+        <div className="top-left">
+          <label>Top Left Radius</label>
+          <br />
+          <input
+            className=""
+            name="topLeftRadius"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="top-right">
+          <label>Top Right Radius</label>
+          <br />
+          <input className="" name="topRightRadius" onChange={handleChange} />
+        </div>
+        <div
+          className="radius-sample"
+          style={{
+            borderTopLeftRadius: radius.topLeftRadius,
+            borderTopRightRadius: radius.topRightRadius,
+            borderBottomLeftRadius: radius.bottomLeftRadius,
+            borderBottomRightRadius: radius.bottomRightRadius,
+          }}
+        ></div>
+        <div className="bottom-left">
+          <label>Bottom Left Radius</label>
+          <br />
+          <input className="" name="bottomLeftRadius" onChange={handleChange}/>
+        </div>
+        <div className="bottom-right">
+          <label>Bottom Right Radius</label>
+          <br />
+          <input className="" name="bottomRightRadius" onChange={handleChange}/>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
-
-export default App
+  );
+};
