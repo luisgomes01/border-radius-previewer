1ce9781034897f231273b5c6cef5ecdf6dd2f2a4
Luís Gomes <luis252001@hotmail.com>
2022-08-31 12:28:04 -0300

    Add copy to clipboard functionality

--- src/App.tsx
+++ src/App.tsx
@@ -10,47 +10,66 @@ export const App: React.FC = () => {
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setRadius({ ...radius, [e.target.name]: e.target.value })
-  }
+    setRadius({ ...radius, [e.target.name]: e.target.value });
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(`border-radius: ${radius.topLeftRadius} ${radius.topRightRadius} ${radius.bottomRightRadius} ${radius.bottomLeftRadius}`);
+    alert('Code copied to clipboard')
+  };
 
   return (
-      <div className="app">
-        <div className="top-left">
-          <label>Top Left Radius</label>
-          <br />
-          <input
-            className="border-radius-input"
-            name="topLeftRadius"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="top-right">
-          <label>Top Right Radius</label>
-          <br />
-          <input className="border-radius-input" name="topRightRadius" onChange={handleChange} />
-        </div>
-        <div
-          className="radius-sample"
-          style={{
-            borderTopLeftRadius: radius.topLeftRadius,
-            borderTopRightRadius: radius.topRightRadius,
-            borderBottomLeftRadius: radius.bottomLeftRadius,
-            borderBottomRightRadius: radius.bottomRightRadius,
-          }}
-        ></div>
-        <div className="bottom-left">
-          <label>Bottom Left Radius</label>
-          <br />
-          <input className="border-radius-input" name="bottomLeftRadius" onChange={handleChange}/>
-        </div>
-        <div className="copy-to-clipboard">
-          <button className="copy-to-clipboard-btn">Copy to clipboard</button>
-        </div>
-        <div className="bottom-right">
-          <label>Bottom Right Radius</label>
-          <br />
-          <input className="border-radius-input" name="bottomRightRadius" onChange={handleChange}/>
-        </div>
+    <div className="app">
+      <div className="top-left">
+        <label>Top Left Radius</label>
+        <br />
+        <input
+          className="border-radius-input"
+          name="topLeftRadius"
+          onChange={handleChange}
+        />
       </div>
+      <div className="top-right">
+        <label>Top Right Radius</label>
+        <br />
+        <input
+          className="border-radius-input"
+          name="topRightRadius"
+          onChange={handleChange}
+        />
+      </div>
+      <div
+        className="radius-sample"
+        style={{
+          borderTopLeftRadius: radius.topLeftRadius,
+          borderTopRightRadius: radius.topRightRadius,
+          borderBottomLeftRadius: radius.bottomLeftRadius,
+          borderBottomRightRadius: radius.bottomRightRadius,
+        }}
+      ></div>
+      <div className="bottom-left">
+        <label>Bottom Left Radius</label>
+        <br />
+        <input
+          className="border-radius-input"
+          name="bottomLeftRadius"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="copy-to-clipboard">
+        <button className="copy-to-clipboard-btn" onClick={copyToClipboard}>
+          Copy code to clipboard
+        </button>
+      </div>
+      <div className="bottom-right">
+        <label>Bottom Right Radius</label>
+        <br />
+        <input
+          className="border-radius-input"
+          name="bottomRightRadius"
+          onChange={handleChange}
+        />
+      </div>
+    </div>
   );
 };
